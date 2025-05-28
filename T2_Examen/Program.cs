6890810c091a4802cf158655616289bfb5a04e53
Leonardo <Leonardo@upn.pe>
2025-05-28 14:53:35 -0500

    Agregar archivos de proyecto.

---
+++ T2_Examen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cajero de 4 opciones
+        }
+    }
+}
