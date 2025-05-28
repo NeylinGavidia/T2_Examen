58c0824b3de38da3a8837bd8cee519abbccc106e
Leonardo <Leonardo@upn.pe>
2025-05-28 15:59:04 -0500

    Arreglado

---
+++ Operaciones/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    public class Class1
+    //comision de 8 soles retiro: no puede dar monedas ni debe ser negativo
+    //comision de deposito 2 soles: se resta a lo que se retira 2 soles de comision
+    //maximo puede retirar 500 soles en operacion;
+    //maximo de deposito 1000;
+    {
+        float re, resi, dep, ad;
+        float monto = 2000;
+
+        public void retiro()
+        {
+            Console.WriteLine("Ingrese el monto a retirar (Costo de la comision S/.8)");
+            re = float.Parse(Console.ReadLine());
+            if (re <= 500)
+            {
+                resi = monto - (re + 8);
+                if (resi >= 0) {
+                    Console.WriteLine("Se ha retirado: " + re + " mas 8 soles de comisión");
+                    Console.WriteLine("El monto actual es: " + resi);
+                }
+                else
+                {
+                    Console.WriteLine("Monto insuficiente");
+                }
+                    monto = resi;
+            }
+            else
+            {
+                Console.WriteLine("La operación no puede ser mayor a S/500");
+            }
+
+        }
+        public void saldo()
+        {
+            Console.WriteLine("El monto actual es:" + monto);
+        }
+        public void deposito()
+        {
+            Console.WriteLine("Ingrese el monto a depositar (Costo de la comision S/.2)");
+            dep = float.Parse(Console.ReadLine());
+            if (dep <= 1000)
+            {
+                ad = monto + (dep - 2);
+                Console.WriteLine("Se ha depositado: " + dep + " menos 2 soles de comisión");
+                Console.WriteLine("El monto actual es: " + ad);
+            }
+            else
+            {
+                Console.WriteLine("La operación no puede ser mayor a S/1000");
+            }
+        }
+    }
+    }
--- T2_Examen/Program.cs
+++ T2_Examen/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Operaciones;
 
 namespace T2_Examen
 {
@@ -10,7 +11,60 @@ namespace T2_Examen
     {
         static void Main(string[] args)
         {
-            //cajero de 4 opciones
+            //cajero de 4 opciones con un while para que solo se muestre hasta el 0
+            //comision de 8 soles retiro: no puede dar monedas ni debe ser negativo
+            //comision de deposito 2 soles: se resta a lo que se retira 2 soles de comision
+            //maximo puede retirar 500 soles en operacion;
+            //maximo de deposito 1000;
+            //se debe poder ver ver el saldo y usar clases y metodos
+            int opcion, respuesta;
+            float monto = 2000;
+
+            Class1 banco = new Class1();
+
+            Console.WriteLine("Bienvenido/a a su banco, elija la operación que quiere realizar.");
+            do
+            {
+                Console.WriteLine("1.retiro, 2.saldo, 3.deposito, 0.salir ");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+
+                {
+
+                    case 0:
+
+                        Console.WriteLine("saliendo....");
+
+                        break;
+
+                    case 1:
+
+                        banco.retiro(); ;
+
+                        break;
+
+                    case 2:
+
+                        banco.saldo();
+
+                        break;
+
+                    case 3:
+
+                        banco.deposito();
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("opcion no valida");
+
+                        break;
+                }
+            }
+            while (opcion != 4 && opcion !=0);
         }
     }
 }
